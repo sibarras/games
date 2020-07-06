957cf0accd3d86b38f9e17d3e6cff17d1ea02af1
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-07-05 19:28:41 -0500

    pake parts done inside LedPrint class

--- 2ndTry.py
+++ 2ndTry.py
@@ -16,3 +16,47 @@ You()
 lst = [1,2,3,'a', 'km']
 
 print(lst.index(3))
+
+class LedPrint:
+    def __init__(self, dimensions=8):
+        self.dimensions = dimensions
+        from time import sleep
+        self.sleep = sleep
+
+    def makeParts(self, snakemouth=tuple, snakebody=list):
+        lastPoint = snakemouth
+        bodyparts = [[]]
+        partNumber = 0
+        if snakebody[1][0] == lastPoint[0]:
+            axis = 'X'
+        elif snakebody[1][1] == lastPoint[1]:
+            axis = 'Y'
+        for point in snakebody:
+            if point[0] == lastPoint[0] and axis == 'X':
+                bodyparts[partNumber].append(point)
+            elif point[1] == lastPoint[1] and axis == 'Y':
+                bodyparts[partNumber].append(point)
+            else:
+                partNumber += 1
+                bodyparts.append([])
+                bodyparts[partNumber].append(point)
+                if axis == 'Y': axis = 'X'
+                elif axis == 'X': axis = 'Y'
+            lastPoint = point
+        return bodyparts
+
+mouth = (4,8)
+body = [
+    (4,8),
+    (4,9),
+    (4,10),
+    (4,11),
+    (5,11),
+    (6,11),
+    (6,10),
+    (6,9),
+    (6,8),
+    (5,8)
+]
+io = LedPrint()
+print(io.makeParts(mouth, body))
